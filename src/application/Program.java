907b15cff7673c215ad802652ba7d07165d7f72c
Alisson <barrosalisson1312@gmail.com>
2023-11-19 10:20:50 -0300

    Add exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,48 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);
 
+		try {
+
+			System.out.print("Room number: ");
+			Integer roomNum = sc.nextInt();
+			System.out.print("Date checkin: ");
+			LocalDate checkin = LocalDate.parse(sc.next(), dateFormat);
+			System.out.print("Date checkout: ");
+			LocalDate checkout = LocalDate.parse(sc.next(), dateFormat);
+
+			Reservation reservation = new Reservation(roomNum, checkin, checkout);
+			System.out.println(reservation.toString());
+
+			System.out.print("Date checkin: ");
+			checkin = LocalDate.parse(sc.next(), dateFormat);
+			System.out.print("Date checkout: ");
+			checkout = LocalDate.parse(sc.next(), dateFormat);
+
+			reservation.updateDates(checkin, checkout);
+			System.out.println(reservation.toString());
+
+		} catch (DateTimeParseException e) { // excepction for parse
+			System.out.println("Invalid Date format");
+		} catch (DomainException e) { // exception personalized
+			System.out.println("Error in reservation: " + e.getMessage());
+		} catch (RuntimeException e) { // generic exception 
+			System.out.println("Unexpected error");
+		}
+
+		sc.close();
 	}
 
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,20 +1,28 @@
 package model.entities;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import model.exceptions.DomainException;
 
 public class Reservation {
 	private Integer roomNumber;
 	private LocalDate checkin;
 	private LocalDate checkout;
 	private DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+
 	public Reservation(Integer roomNumber, LocalDate checkin, LocalDate checkout) {
-		super();
+		if (checkin.isBefore(LocalDate.now()) || checkout.isBefore(LocalDate.now())) {
+			throw new DomainException("Reservations dates for update must be futures dates");
+		}
+		if (checkout.isBefore(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+
+		}
 		this.roomNumber = roomNumber;
-		this.checkin = checkin;
-		this.checkout = checkout;
+		setCheckin(checkin);
+		setCheckout(checkout);
 	}
 
 	public Integer getRoomNumber() {
@@ -28,32 +36,35 @@ public class Reservation {
 	public LocalDate getCheckout() {
 		return checkout;
 	}
+
 	private void setCheckin(LocalDate checkin) {
 		this.checkin = checkin;
 	}
+
 	private void setCheckout(LocalDate checkout) {
 		this.checkout = checkout;
 	}
-	
+
 	public long duration() {
-		Duration diff = Duration.between(checkin, checkout); // calculo para obter a diferença das datas
-		return diff.toDays(); // retorna a diferenca de datas em dias
+		return ChronoUnit.DAYS.between(checkin, checkout);
 	}
+
 	public void updateDates(LocalDate checkin, LocalDate checkout) {
+		if (checkin.isBefore(LocalDate.now()) || checkout.isBefore(LocalDate.now())) {
+			throw new DomainException("Reservations dates for update must be futures dates");
+		}
+		if (checkout.isBefore(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+
+		}
 		setCheckin(checkin);
 		setCheckout(checkout);
+
 	}
 
 	public String toString() {
-		return "Reservation: Room "
-				+ getRoomNumber()
-				+ ", check in: "
-				+ dateFormat.format(checkin)
-				+ ", checkout: "
-				+ dateFormat.format(checkout)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Reservation: Room " + getRoomNumber() + ", check in: " + dateFormat.format(checkin) + ", checkout: "
+				+ dateFormat.format(checkout) + ", " + duration() + " nights";
 	}
 
 }
